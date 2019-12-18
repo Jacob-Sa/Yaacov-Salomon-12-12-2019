40c3a2a1e7c05641e3bf0d1ed656f9e134664f62
Yaacov Salomon <salomonyaacov@gmail.com>
2019-12-18 17:33:34 +0200

    final commit

---
+++ src/components/api/acuuweather/config.js
@@ -0,0 +1,11 @@
+
+
+export const apiKey = 'K0isUWYTkpBuRvpRNRE0EuIVIZEAOnuL';
+export const endPoints = {
+    geoposition: `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=`,
+    locationKey: 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/',
+    autocompleteSearch: `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=`,
+    currentConditions: `https://dataservice.accuweather.com/currentconditions/v1/`,
+};
+
+//Z4KPevJp0MDfG0b5GDxnJPnda24ExpSS
